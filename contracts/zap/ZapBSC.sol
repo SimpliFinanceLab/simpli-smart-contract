fad2d06db076d614088987dfd1546e595a69b013
Tom <smalapet@gmail.com>
2021-09-30 22:47:59 +0700

    Smart contract source code

---
+++ contracts/zap/ZapBSC.sol
@@ -0,0 +1,302 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+import "../math/SafeMath.sol";
+import "../token/SafeBEP20.sol";
+import "../utils/OwnableUpgradeable.sol";
+import "../interfaces/IPancakePair.sol";
+import "../interfaces/IPancakeRouter02.sol";
+import "../interfaces/IZap.sol";
+import "../interfaces/ISafeSwapBNB.sol";
+
+contract ZapBSC is IZap, OwnableUpgradeable {
+    using SafeMath for uint;
+    using SafeBEP20 for IBEP20;
+
+    /* ========== CONSTANT VARIABLES ========== */
+
+    address private constant CAKE = 0x1A78Af71E79A2183442dD9cb309580F425Cad962;
+    address private constant WBNB = 0x6945bA4c180e431334291e48C858C82c5B7B6387;
+    address private constant BUSD = 0xA4748C70A88D8a10A6B29c35AD116d0ad91Ee0Ab;
+    address private constant USDT = 0x240Aff20c537169eecC3Dca741819Ac583837Ef3;
+
+
+    IPancakeRouter02 private constant ROUTER = IPancakeRouter02(0x8149f34D760f3683547b701DcEc4255fed847AD3);
+
+    /* ========== STATE VARIABLES ========== */
+
+    mapping(address => bool) private notFlip;
+    mapping(address => address) private routePairAddresses;
+    address[] public tokens;
+    address public safeSwapBNB;
+
+    /* ========== INITIALIZER ========== */
+
+    function initialize() external initializer {
+        __Ownable_init();
+        require(owner() != address(0), "Zap: owner must be set");
+
+        setNotFlip(CAKE);
+        setNotFlip(WBNB);
+        setNotFlip(BUSD);
+        setNotFlip(USDT);
+
+        /*
+        CASE 1: WBNB -> USDT/BUSD -> LP
+        CASE 2: USDT -> USDT/BUSD -> LP
+        CASE 3: USDT -> CAKE
+        CASE 4: USDT -> CAKE/USDT
+        */
+        setRoutePairAddress(USDT, BUSD);
+    }
+
+    receive() external payable {}
+
+
+    /* ========== View Functions ========== */
+
+    function isFlip(address _address) public view returns (bool) {
+        return !notFlip[_address];
+    }
+
+    function routePair(address _address) external view returns(address) {
+        return routePairAddresses[_address];
+    }
+
+    /* ========== External Functions ========== */
+
+    function zapInToken(address _from, uint amount, address _to) external override {
+        IBEP20(_from).safeTransferFrom(msg.sender, address(this), amount);
+        _approveTokenIfNeeded(_from);
+
+        if (isFlip(_to)) {
+            IPancakePair pair = IPancakePair(_to);
+            address token0 = pair.token0();
+            address token1 = pair.token1();
+            if (_from == token0 || _from == token1) {
+                // swap half amount for other
+                address other = _from == token0 ? token1 : token0;
+                _approveTokenIfNeeded(other);
+                uint sellAmount = amount.div(2);
+                uint otherAmount = _swap(_from, sellAmount, other, address(this));
+                ROUTER.addLiquidity(_from, other, amount.sub(sellAmount), otherAmount, 0, 0, msg.sender, block.timestamp);
+            } else {
+                uint bnbAmount = _from == WBNB ? _safeSwapToBNB(amount) : _swapTokenForBNB(_from, amount, address(this));
+                _swapBNBToFlip(_to, bnbAmount, msg.sender);
+            }
+        } else {
+            _swap(_from, amount, _to, msg.sender);
+        }
+    }
+
+    function zapIn(address _to) external payable override {
+        _swapBNBToFlip(_to, msg.value, msg.sender);
+    }
+
+    function zapOut(address _from, uint amount) external override {
+        IBEP20(_from).safeTransferFrom(msg.sender, address(this), amount);
+        _approveTokenIfNeeded(_from);
+
+        if (!isFlip(_from)) {
+            _swapTokenForBNB(_from, amount, msg.sender);
+        } else {
+            IPancakePair pair = IPancakePair(_from);
+            address token0 = pair.token0();
+            address token1 = pair.token1();
+            if (token0 == WBNB || token1 == WBNB) {
+                ROUTER.removeLiquidityETH(token0 != WBNB ? token0 : token1, amount, 0, 0, msg.sender, block.timestamp);
+            } else {
+                ROUTER.removeLiquidity(token0, token1, amount, 0, 0, msg.sender, block.timestamp);
+            }
+        }
+    }
+
+    /* ========== Private Functions ========== */
+
+    function _approveTokenIfNeeded(address token) private {
+        if (IBEP20(token).allowance(address(this), address(ROUTER)) == 0) {
+            IBEP20(token).safeApprove(address(ROUTER), uint(- 1));
+        }
+    }
+
+    function _swapBNBToFlip(address flip, uint amount, address receiver) private {
+        if (!isFlip(flip)) {
+            _swapBNBForToken(flip, amount, receiver);
+        } else {
+            // flip
+            IPancakePair pair = IPancakePair(flip);
+            address token0 = pair.token0();
+            address token1 = pair.token1();
+            if (token0 == WBNB || token1 == WBNB) {
+                address token = token0 == WBNB ? token1 : token0;
+                uint swapValue = amount.div(2);
+                uint tokenAmount = _swapBNBForToken(token, swapValue, address(this));
+
+                _approveTokenIfNeeded(token);
+                ROUTER.addLiquidityETH{value : amount.sub(swapValue)}(token, tokenAmount, 0, 0, receiver, block.timestamp);
+            } else {
+                uint swapValue = amount.div(2);
+                uint token0Amount = _swapBNBForToken(token0, swapValue, address(this));
+                uint token1Amount = _swapBNBForToken(token1, amount.sub(swapValue), address(this));
+
+                _approveTokenIfNeeded(token0);
+                _approveTokenIfNeeded(token1);
+                ROUTER.addLiquidity(token0, token1, token0Amount, token1Amount, 0, 0, receiver, block.timestamp);
+            }
+        }
+    }
+
+    function _swapBNBForToken(address token, uint value, address receiver) private returns (uint) {
+        address[] memory path;
+
+        if (routePairAddresses[token] != address(0)) {
+            path = new address[](3);
+            path[0] = WBNB;
+            path[1] = routePairAddresses[token];
+            path[2] = token;
+        } else {
+            path = new address[](2);
+            path[0] = WBNB;
+            path[1] = token;
+        }
+
+        uint[] memory amounts = ROUTER.swapExactETHForTokens{value : value}(0, path, receiver, block.timestamp);
+        return amounts[amounts.length - 1];
+    }
+
+    function _swapTokenForBNB(address token, uint amount, address receiver) private returns (uint) {
+        address[] memory path;
+        if (routePairAddresses[token] != address(0)) {
+            path = new address[](3);
+            path[0] = token;
+            path[1] = routePairAddresses[token];
+            path[2] = WBNB;
+        } else {
+            path = new address[](2);
+            path[0] = token;
+            path[1] = WBNB;
+        }
+
+        uint[] memory amounts = ROUTER.swapExactTokensForETH(amount, 0, path, receiver, block.timestamp);
+        return amounts[amounts.length - 1];
+    }
+
+    function _swap(address _from, uint amount, address _to, address receiver) private returns (uint) {
+        address intermediate = routePairAddresses[_from];
+        if (intermediate == address(0)) {
+            intermediate = routePairAddresses[_to];
+        }
+
+        address[] memory path;
+        if (intermediate != address(0) && (_from == WBNB || _to == WBNB)) {
+            // [WBNB, BUSD, VAI] or [VAI, BUSD, WBNB]
+            path = new address[](3);
+            path[0] = _from;
+            path[1] = intermediate;
+            path[2] = _to;
+        } else if (intermediate != address(0) && (_from == intermediate || _to == intermediate)) {
+            // [VAI, BUSD] or [BUSD, VAI]
+            path = new address[](2);
+            path[0] = _from;
+            path[1] = _to;
+        } else if (intermediate != address(0) && routePairAddresses[_from] == routePairAddresses[_to]) {
+            // [VAI, DAI] or [VAI, USDC]
+            path = new address[](3);
+            path[0] = _from;
+            path[1] = intermediate;
+            path[2] = _to;
+        } else if (routePairAddresses[_from] != address(0) && routePairAddresses[_to] != address(0) && routePairAddresses[_from] != routePairAddresses[_to]) {
+            // routePairAddresses[xToken] = xRoute
+            // [VAI, BUSD, WBNB, xRoute, xToken]
+            path = new address[](5);
+            path[0] = _from;
+            path[1] = routePairAddresses[_from];
+            path[2] = WBNB;
+            path[3] = routePairAddresses[_to];
+            path[4] = _to;
+        } else if (intermediate != address(0) && routePairAddresses[_from] != address(0)) {
+            // [VAI, BUSD, WBNB, BUNNY]
+            path = new address[](4);
+            path[0] = _from;
+            path[1] = intermediate;
+            path[2] = WBNB;
+            path[3] = _to;
+        } else if (intermediate != address(0) && routePairAddresses[_to] != address(0)) {
+            // [BUNNY, WBNB, BUSD, VAI]
+            path = new address[](4);
+            path[0] = _from;
+            path[1] = WBNB;
+            path[2] = intermediate;
+            path[3] = _to;
+        } else if (_from == WBNB || _to == WBNB) {
+            // [WBNB, BUNNY] or [BUNNY, WBNB]
+            path = new address[](2);
+            path[0] = _from;
+            path[1] = _to;
+        } else {
+            // [USDT, BUNNY] or [BUNNY, USDT]
+            path = new address[](3);
+            path[0] = _from;
+            path[1] = WBNB;
+            path[2] = _to;
+        }
+
+        uint[] memory amounts = ROUTER.swapExactTokensForTokens(amount, 0, path, receiver, block.timestamp);
+        return amounts[amounts.length - 1];
+    }
+
+    function _safeSwapToBNB(uint amount) private returns (uint) {
+        require(IBEP20(WBNB).balanceOf(address(this)) >= amount, "Zap: Not enough WBNB balance");
+        require(safeSwapBNB != address(0), "Zap: safeSwapBNB is not set");
+        uint beforeBNB = address(this).balance;
+        ISafeSwapBNB(safeSwapBNB).withdraw(amount);
+        return (address(this).balance).sub(beforeBNB);
+    }
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    function setRoutePairAddress(address asset, address route) public onlyOwner {
+        routePairAddresses[asset] = route;
+    }
+
+    function setNotFlip(address token) public onlyOwner {
+        bool needPush = notFlip[token] == false;
+        notFlip[token] = true;
+        if (needPush) {
+            tokens.push(token);
+        }
+    }
+
+    function removeToken(uint i) external onlyOwner {
+        address token = tokens[i];
+        notFlip[token] = false;
+        tokens[i] = tokens[tokens.length - 1];
+        tokens.pop();
+    }
+
+    function sweep() external onlyOwner {
+        for (uint i = 0; i < tokens.length; i++) {
+            address token = tokens[i];
+            if (token == address(0)) continue;
+            uint amount = IBEP20(token).balanceOf(address(this));
+            if (amount > 0) {
+                _swapTokenForBNB(token, amount, owner());
+            }
+        }
+    }
+
+    function withdraw(address token) external onlyOwner {
+        if (token == address(0)) {
+            payable(owner()).transfer(address(this).balance);
+            return;
+        }
+
+        IBEP20(token).transfer(owner(), IBEP20(token).balanceOf(address(this)));
+    }
+
+    function setSafeSwapBNB(address _safeSwapBNB) external onlyOwner {
+        require(safeSwapBNB == address(0), "Zap: safeSwapBNB already set!");
+        safeSwapBNB = _safeSwapBNB;
+        IBEP20(WBNB).approve(_safeSwapBNB, uint(-1));
+    }
+}
